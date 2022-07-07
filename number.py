15ec32a19c2f79936cefb62545b886de7ad19337
Mark Dijkhof <markdijkhof66@gmail.com>
2022-07-07 16:49:56 +0200

    commit on 'Python'

--- number.py
+++ number.py
@@ -1,6 +1,19 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
-    print(f"x = {x}")
+def main():
+    x = get_int_v2()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+
+def get_int_v2():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            pass
+
+main()
